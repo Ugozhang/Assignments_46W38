179d538e76ce3cb41f77bb8183a313bb5ae50f1f
UgoZhang <sleepychang@gmail.com>
2025-09-22 23:12:06 +0800

    sorting function added

--- module03/min-max_finder.py
+++ module03/min-max_finder.py
@@ -34,3 +34,43 @@ while i<len(num_list):
 print("The list of number is:", num_list)
 print("The minimal number is:", min_inlist)
 print("The maximal number is:", max_inlist)
+
+# sorting
+def QuickSort_byBFC_asc(s):
+    n = len(s)
+    if n<=1:
+        return s
+    left=[]
+    right=[]
+    pivot = float(s[0])
+    for i in range(1,n):
+        if float(s[i]) < float(pivot):
+            left.append(s[i])
+        else:
+            right.append(s[i])
+    return QuickSort_byBFC_asc(left) + [pivot] + QuickSort_byBFC_asc(right)
+def QuickSort_byBFC_dsc(s):
+    n = len(s)
+    if n<=1:
+        return s
+    left=[]
+    right=[]
+    pivot = float(s[0])
+    for i in range(1,n):
+        if float(s[i]) > float(pivot):
+            left.append(s[i])
+        else:
+            right.append(s[i])
+    return QuickSort_byBFC_dsc(left) + [pivot] + QuickSort_byBFC_dsc(right)
+#while sorted list <> nan
+list_sorted = []
+while len(list_sorted) == 0:
+    sortby_boolean = input("Sorting List by Asceding(True), or descending(False)? =>")
+    if sortby_boolean == "1" or sortby_boolean == "True" or sortby_boolean == "true" or sortby_boolean == "T" or sortby_boolean == "t":
+        list_sorted = QuickSort_byBFC_asc(num_list)
+    elif sortby_boolean == "0" or sortby_boolean == "False" or sortby_boolean == "false" or sortby_boolean == "F" or sortby_boolean == "f":
+        list_sorted = QuickSort_byBFC_dsc(num_list)
+    else:
+        print("Wrong typing")
+
+print("The list of number sorted:", list_sorted)
